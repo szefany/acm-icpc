64610ca483849d083fc53ca31febccbae7ab1218
szefany <szefany@gmail.com>
2013-02-14 22:26:36 +0800

    stoi-2012

---
+++ training/my-training/stoi-2012/senior/A.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+#include <cstring>
+
+const int MOD = 1000000000 + 7;
+
+int a[100000];
+int n;
+
+int inverse (int a) {
+    return a == 1 ? 1 : (long long)(MOD - MOD / a) * inverse (MOD % a) % MOD;
+}
+
+int main () {
+    scanf ("%d", &n);
+    for (int i = 0; i < n; ++ i) {
+        scanf ("%d", &a[i]);
+    }
+    int result = 1;
+    for (int i = 0; i < n; ++ i) {
+        result = (long long)result * a[i] % MOD;
+        a[i] = inverse (a[i]);
+    }
+    for (int i = 0; i < n; ++ i) {
+        int answer = (long long)result * a[i] % MOD;
+        printf ("%d%c", answer, i == n - 1 ? '\n' : ' ');
+    }
+    return 0;
+}
