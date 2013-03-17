63e493f3e6e01beeacaa4cc065f27223e0a06e84
szefany <szefany@gmail.com>
2013-03-17 14:53:49 +0800

    Codeforces Round #166 (Div. 2) D

--- codeforces/271-Codeforces Round #166 (Div. 2)/D.cpp
+++ codeforces/271-Codeforces Round #166 (Div. 2)/D.cpp
@@ -1,45 +1,100 @@
-#include <iostream>
 #include <cstdio>
 #include <cstring>
-#include <map>
-using namespace std;
+#include <iostream>
 
-char str[1500 + 5];
-bool bad[26];
-int n;
+typedef int Matrix[2][2];
 
-const int HASH = 9999991;
-typedef pair <unsigned long long, int> Hash;
-map <Hash, int> counter;
+const int table[4] = {1000000007, 222222224, 183120, 240};
+const Matrix args = {{3, 1}, {1, 0}};
+const Matrix init = {{1, 0}, {0, 0}};
 
-int main () {
-    char buffer[1500 + 5];
-    scanf ("%s", str);
-    scanf ("%s", buffer);
-    for (int i = 0; i < 26; ++ i) {
-        if (buffer[i] == '0') {
-            bad[i] = true;
-        }
+struct data {
+    Matrix matrix;
+    int n, m;
+    data(): n(0), m(0) {
+        memset(matrix, 0, sizeof(matrix));
     }
-    int length = strlen (str), k;
-    scanf ("%d", &k);
-    for (int i = 0; i < length; ++ i) {
-        int sum = 0;
-        Hash hash = make_pair (0, 0);
-        for (int j = i; j < length; ++ j) {
-            hash.first = hash.first * 131 + str[j];
-            hash.second = ((long long)hash.second * 131 + str[j]) % HASH;
-            sum += bad[str[j] - 'a'];
-            if (sum <= k) {
-                counter[hash] ++;
-            }
-            else {
-                break;
+    data(const Matrix &other, int n, int m): n(n), m(m) {
+        for (int i = 0; i < n; ++ i) {
+            for (int j = 0; j < m; ++ j) {
+                matrix[i][j] = other[i][j];
             }
         }
     }
-    int answer = 0;
-    for (map <Hash, int>::iterator iter = counter.begin(); iter != counter.end(); iter ++, answer ++);
-    cout << answer << endl;
+    void diagonal(int size) {
+        n = m = size;
+        for (int i = 0; i < size; ++ i) {
+            for (int j = 0; j < size; ++ j) {
+                matrix[i][j] = i == j ? 1 : 0;
+            }
+        }
+    }
+};
+
+Matrix buffer;
+void multi(data &base, data &other, int MOD) {
+    memset(buffer, 0, sizeof(buffer));
+    for (int i = 0; i < base.n; ++ i) {
+        for (int j = 0; j < base.m; ++ j) {
+            if (base.matrix[i][j]) {
+                for (int k = 0; k < other.m; ++ k) {
+                    buffer[i][k] += (long long)base.matrix[i][j] * other.matrix[j][k] % MOD;
+                    if (buffer[i][k] > MOD) {
+                        buffer[i][k] -= MOD;
+                    }
+                }
+            }
+        }
+    }
+    base = data(buffer, base.n, other.m);
+}
+
+void power(data &base, long long times, int MOD) {
+    data result;
+    result.diagonal(base.n);
+    while (times) {
+        int x = times & 1;
+        if (x) {
+            multi(result, base, MOD);
+        }
+        multi(base, base, MOD);
+        times >>= 1;
+    }
+    base = result;
+}
+
+long long solve(long long n, int MOD) {
+    if (n < 3) {
+        return n == 2 ? 3 : n; 
+    }
+    data base(args, 2, 2), target(init, 2, 1);
+    power(base, n - 1, MOD);
+    multi(base, target, MOD);
+    return base.matrix[0][0];
+}
+
+int main() {
+    /*
+    long long a[2] = {0, 1};
+    for (int i = 2; ; ++ i) {
+        long long temp = (a[0] + a[1] * 3) % MOD;
+        if (temp == 1 && a[1] == 0) {
+            printf ("%d\n", i - 1);
+            break;
+        }
+        a[0] = a[1], a[1] = temp;
+    }
+    */
+    freopen("input", "r", stdin);
+    int test_count;
+    scanf("%d", &test_count);
+    while (test_count --) {
+        long long n;
+        std::cin >> n;
+        for (int i = 3; i >= 0; -- i) {
+            n = solve(n, table[i]);
+        }
+        std::cout << n << '\n';
+    }
     return 0;
 }
