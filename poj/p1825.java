9b057fcf7e214b6f0138b78dd72784498183bc8a
szefany <szefany@gmail.com>
2013-05-19 13:17:14 +0800

    POJ 1825 Young

---
+++ poj/p1825.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class Main {
+    static int[] count = new int[30];
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n, sum = 0;
+        n = in.nextInt();
+        for (int i = 1; i <= n; ++ i) {
+            count[i] = in.nextInt();
+            sum += count[i];
+        }
+        BigInteger answer = BigInteger.ONE;
+        for (int i = 1; i <= sum; ++ i) {
+            answer = answer.multiply(BigInteger.valueOf(i));
+        }
+        for (int i = 1; i <= n; ++ i) {
+            for (int j = 1; j <= count[i]; ++ j) {
+                int temp = count[i] - j + 1;
+                for (int k = i + 1; k <= n; ++ k) {
+                    if (count[k] >= j) {
+                        temp ++;
+                    }
+                }
+                answer = answer.divide(BigInteger.valueOf(temp));
+            }
+        }
+        System.out.println(answer);
+    }
+};
