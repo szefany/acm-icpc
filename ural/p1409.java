f229aab3190a3caf6b9cd3b272637c5f944c5044
szefany <szefany@gmail.com>
2013-05-20 16:00:02 +0800

    URAL 1409 Two Gangsters

---
+++ ural/p1409.java
@@ -0,0 +1,62 @@
+import java.util.*;
+import java.math.*;
+import java.io.*;
+
+public class Main {
+
+    void start() throws IOException {
+        int a = reader.nextInt();
+        int b = reader.nextInt();
+        writer.println((b - 1) + " " + (a - 1));
+    }
+
+    public void run() {
+        try {
+            start();
+        } catch (IOException ex) {
+        }
+        writer.flush();
+        writer.close();
+    }
+    
+    InputReader reader;
+    PrintWriter writer;
+    
+    Main() {
+        reader = new InputReader();
+        writer = new PrintWriter(System.out);
+    }
+
+    public static void main(String args[]) {
+        new Main().run();
+    }
+}
+
+class InputReader {
+    BufferedReader reader;
+    StringTokenizer tokenizer;
+
+    InputReader() {
+        reader = new BufferedReader(new InputStreamReader(System.in));
+        tokenizer = new StringTokenizer("");
+    }
+    
+    String next() throws IOException {
+        while (!tokenizer.hasMoreTokens()) {
+            tokenizer = new StringTokenizer(reader.readLine());
+        }
+        return tokenizer.nextToken();
+    }
+
+    Integer nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+    
+    Long nextLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    Double nextDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+}
